96913e45f69f3c9e5a561974de6202a18dee67ae
Bianca Fu <0509biancafu@gmail.com>
2022-11-22 23:21:19 +0000

    create a spinner

--- spinner1.js
+++ spinner1.js
@@ -28,14 +28,11 @@ setTimeout(() => {
 }, 1300);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 1500);
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|   '); 
 }, 1700);
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\n'); 
 }, 1900);
